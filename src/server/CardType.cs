91cfebe702a864c454262080724af06540eb6a15
ZaitsevSergei <zaitsev.sergey.93@gmail.com>
2018-06-27 23:13:29 +0300

    Enum for card type created

---
+++ src/server/CardType.cs
@@ -0,0 +1,10 @@
+﻿namespace Server
+{
+    /// <summary>
+    /// Enumeration with card types
+    /// </summary>
+    public enum CardType
+    {
+        Invalid, Mastercard, Visa, Maestro, Electron
+    }
+}
