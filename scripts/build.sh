edaec94cccf31e2c66e0bd4a400dd7439081544c
Nikolay Molchanov <me@kroniak.net>
2018-07-01 11:31:14 +0300

    [add] build && test scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,18 @@
-dotnet restore ../src/server/
-dotnet build -c Release ../src/server/
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+command -v dotnet >/dev/null 2>&1 || {
+    echo >&2 "This script requires the dotnet core sdk tooling to be installed"
+    exit 1
+}
+
+echo "!!WARNING!! This script only builds netstandard and netcoreapp targets"
+echo "!!WARNING!! Do not publish nupkgs generated from this script"
+
+dotnet --info
+
+dotnet restore -v m "${SCRIPT_ROOT}/../src/server"
+
+dotnet build -c Release "${SCRIPT_ROOT}/../src/server/"
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,6 @@
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=../../lcov ../test/server/
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+dotnet test -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput="${SCRIPT_ROOT}/../lcov" "${SCRIPT_ROOT}/../test/server/"
