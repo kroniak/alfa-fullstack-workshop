c027a754a71979e922fb78dab8ff26fc82faa191
Irina Soloveva <aniri.solovyeva@gmail.com>
2018-07-03 01:52:10 +0300

    Add card, transaction and user models

---
+++ src/server/Infrastructure/CurrencyTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Infrastructure
+{
+    /// <summary>
+    /// Enum for currency
+    /// </summary>
+    public enum CurrencyTypes
+    {
+        Ruble,
+        Dollar
+    }
+}
---
+++ src/server/Models/Repository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Server.Models
+{
+    public static class Repository
+    {
+        public static List<User> Users { get; } = new List<User>();
+
+        public static List<Card> Cards { get; } = new List<Card>();
+        
+        public static List<Transaction> Transactions { get; } = new List<Transaction>();
+
+        /*public static void AddUser(User user)
+        {
+            Users.Add(user);
+        }
+        
+        public static void AddCard(Card card)
+        {
+            Cards.Add(card);
+        }
+        
+        public static void AddTransaction(Transaction transaction)
+        {
+            Transactions.Add(transaction);
+        }*/
+    }
+}
