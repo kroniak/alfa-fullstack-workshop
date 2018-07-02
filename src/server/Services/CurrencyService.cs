30148809c3e68d17994f2e759380e021a4183c4c
Irina Soloveva <aniri.solovyeva@gmail.com>
2018-07-03 01:53:29 +0300

    Add services for user and currency

---
+++ src/server/Services/CurrencyService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Server.Infrastructure;
+
+namespace Server.Services
+{
+    public static class CurrencyService
+    {
+        public static decimal CurrencyConversion(CurrencyTypes from, decimal amount)
+        {
+            Random rnd = new Random();
+            decimal rateRubleInDollar = (decimal)rnd.Next(5000, 6000)/100;
+
+            switch (from)
+            {
+                case CurrencyTypes.Dollar:
+                    return amount * rateRubleInDollar;
+                case CurrencyTypes.Ruble:
+                    return amount / rateRubleInDollar;
+                default:
+                    throw new Exception("Currency type is incorrect");
+            }
+        }
+    }
+}
---
+++ src/server/Services/UserService.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+using Server.Exceptions;
+using Server.Infrastructure;
+
+namespace Server.Services
+{
+    public static class UserService
+    {
+        public static void CheckUserName(string userName)
+        {
+            const int maxLengthUserName = 15,
+                minLengthUserName = 4;
+
+            if (string.IsNullOrEmpty(userName))
+                throw new UserDataException("username is null or empty", UserExceptionTypes.UserName, userName);
+
+            if (userName.Length < minLengthUserName)
+                throw new UserDataException("username length is too short", UserExceptionTypes.UserName, userName);
+
+            if (userName.Length > maxLengthUserName)
+                throw new UserDataException("username length is too long", UserExceptionTypes.UserName, userName);
+
+            string pattern = @"[a-z|0-9]+[a-z|0-9|\.|_|@|-]+[a-z|0-9]+";
+
+            Match match = Regex.Match(userName, pattern, RegexOptions.IgnoreCase);
+
+            if (!match.Success)
+            {
+                throw new UserDataException("username contains incorrect symbols", UserExceptionTypes.UserName,userName);
+            }
+        }
+
+        public static void CheckPassword(string password)
+        {
+            const int maxLengthPassword = 20,
+                minLengthPassword = 7;
+            
+            if (string.IsNullOrEmpty(password))
+                throw new UserDataException("password is null or empty", UserExceptionTypes.Password, password);
+            
+            if (password.Length < minLengthPassword)
+                throw new UserDataException("password is too short", UserExceptionTypes.Password, password);
+            
+            if (password.Length > maxLengthPassword)
+                throw new UserDataException("password is too long", UserExceptionTypes.Password, password);
+        }
+    }
+}
